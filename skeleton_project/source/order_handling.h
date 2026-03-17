e152f57a8297fa075c8f569bf837bd3216b30a4c
Fredrik <71010718+frs25frs25@users.noreply.github.com>
2026-03-17 21:01:44 +0100

    Update order_handling.h

--- skeleton_project/source/order_handling.h
+++ skeleton_project/source/order_handling.h
@@ -21,5 +21,6 @@ void remove_order(Order *order_ptr,int order_to_delete_index);
 void remove_all_orders(Order *order_ptr);
 void remove_orders_with_same_floor(Order *order_ptr, int last_floor);
 void jump_the_queue(Order* forder, Order* lorder, int last_floor);
+void print_order_array(Order *order_ptr);
 #endif
 
